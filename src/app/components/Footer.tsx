9c64170e15490ff9d6309ef95c4a2e55e6eb7b44
Markel-Bradford <markel.bradford94@gmail.com>
2024-08-04 15:17:29 -0500

    Adjusted footer text size

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
 
   return (
     <div className="relative bg-black pt-[10px] border-t-2 border-t-solid border-t-amber-400 flex justify-center items-center h-[80px] font-bold text-amber-400">
-        <p>&copy; Elevated Events, 2024 | Made by <a href="https://markel-bradford.github.io/devportfolio/" target="_blank"><span className="text-amber-600 hover:text-white">Markel Bradford</span></a></p>
+        <p className="max-md:text-sm">&copy; Elevated Events, 2024 | Made by <a href="https://markel-bradford.github.io/devportfolio/" target="_blank"><span className="text-amber-600 hover:text-white">Markel Bradford</span></a></p>
     </div>
   );
 };
