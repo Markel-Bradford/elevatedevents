0cf22a37ec2e8c3e79aae020632e78be8c36ad09
Markel-Bradford <markel.bradford94@gmail.com>
2024-08-19 19:58:52 -0500

    Completed api setup for mailgun

---
+++ src/app/api/send-email/route.ts
@@ -0,0 +1,59 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import Mailgun, {MailgunMessageData} from 'mailgun.js';
+import FormData from 'form-data'
+import Mailgen from 'mailgen';
+
+const API_KEY = process.env.MAILGUN_KEY;
+const DOMAIN = process.env.MAILGUN_DOMAIN;
+
+const mailgen = new Mailgen({
+    theme: 'default',
+    product: {
+        name: 'Elevated Events',
+        link: 'https://elevatedevents.vercel.app'
+    }
+})
+
+const mailgun = new Mailgun(FormData).client({
+    username: 'api',
+    key: API_KEY
+})
+
+type Data ={
+    success: boolean;
+}
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<Data>
+) {
+    if (req.method === 'POST'){
+        const body = req.body || {}
+        const intro = body.intro || ''
+        const content = body.content
+        const email = {
+            body: {
+                name: body.name || 'Customer',
+                intro,
+                outro: content,
+            }
+        }
+
+        try {
+            await mailgun.messages.create(DOMAIN, {
+                to: body.to,
+                from: 'Elevated Events <no-reply@elevatedevents.com>',
+                subject: body.subject || 'Email',
+                text: mailgen.generatePlaintext(email),
+                html: mailgen.generate(email),
+            })
+            res.status(200).json({success: true})
+        } catch (e) {
+            console.error("Error sending email:", e)
+            res.status(500).json({success: false})
+        }
+
+        return
+    }
+    res.status(404).json({success: false})
+}
--- src/app/components/EmailBtn.tsx
+++ src/app/components/EmailBtn.tsx
@@ -1,49 +1,48 @@
-"use client";  // Add this line at the top of your component file
+"use client"; // Add this line at the top of your component file
 
 import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
-
 const EmailBtn: React.FC = () => {
   const pathname = usePathname();
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
     try {
-        const response = await fetch('/api/send-email', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                name: 'Markel',
-                to: 'markel.bradford94@gmail.com',
-                subject: 'Package Info',
-                intro: 'Thanks for contacting Elevated Events',
-                content: 'Hello, I would like to know more about your silver event. I look forward to hearing from you soon!'
-            }),
-        });
-        if (response.ok) {
-            alert('Email sent successfully')
-        } else {
-            alert('Failed to send email.')
-        }
-    } catch (error) {
-        console.error('Error sending email:', error)
-        alert('Failed to send email.')
-    }
-  }
+      const r = await fetch("/api/send-email", {
+        method: "POST",
+        body: JSON.stringify({
+          name: "Markel",
+          to: "markel.bradford94@gmail.com",
+          subject: "Package Info",
+          intro: "Thanks for contacting Elevated Events",
+          content:
+            "Hello, I would like to know more about your silver event. I look forward to hearing from you soon!",
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
 
-  return (  
+      const res = await r.json();
+
+      if (res.success) {
+        alert("Email sent successfully");
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      alert("Failed to send email.");
+    }
+  };
+
+  return (
     <div className="flex bg-black relative items-center justify-center">
       <button
         onClick={handleSubmit}
-        type="submit"
-        className="bg-amber-400 p-4 font-semibold text-lg shadow-slate-200 shadow-sm hover:scale-105"
-      >
+        className="bg-amber-400 p-4 font-semibold text-lg shadow-slate-200 shadow-sm hover:scale-105">
         Request More Info
       </button>
-</div>
+    </div>
   );
 };
 
