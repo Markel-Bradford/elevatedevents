9580336d2467cf7c7f2727fcfab89ed8f0346ed5
Markel-Bradford <markel.bradford94@gmail.com>
2024-08-20 17:13:43 -0500

    Commiting changes

--- src/app/components/EmailBtn.tsx
+++ src/app/components/EmailBtn.tsx
@@ -4,20 +4,51 @@ import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 const EmailBtn: React.FC = () => {
-//   const pathname = usePathname();
+  const pathname = usePathname();
 
   const handleSubmit = async () => {
-    try {
-      const r = await fetch("/api/sendemail", {
-        method: "POST",
-        body: JSON.stringify({
+    let emailDetails;
+
+    switch (pathname) {
+      case "/silver":
+        emailDetails = {
           name: "Markel",
           to: "markel.bradford94@gmail.com",
           subject: "Package Info",
           intro: "Thanks for contacting Elevated Events",
           content:
-            "Hello, I would like to know more about your silver event. I look forward to hearing from you soon!",
-        }),
+            "Hello, I would like to know more about your silver event package. I look forward to hearing from you soon!",
+        }
+        break
+
+        case "/gold":
+        emailDetails = {
+          name: "Markel",
+          to: "markel.bradford94@gmail.com",
+          subject: "Package Info",
+          intro: "Thanks for contacting Elevated Events",
+          content:
+            "Hello, I would like to know more about your gold event package. I look forward to hearing from you soon!",
+        }
+        break
+
+        case "/platinum":
+        emailDetails = {
+          name: "Markel",
+          to: "markel.bradford94@gmail.com",
+          subject: "Package Info",
+          intro: "Thanks for contacting Elevated Events",
+          content:
+            "Hello, I would like to know more about your platinum event package. I look forward to hearing from you soon!",
+        }
+        break
+    }
+    
+    try {
+      const r = await fetch("/api/sendemail", {
+        
+        method: "POST",
+        body: JSON.stringify(emailDetails),
         headers: {
           "Content-Type": "application/json",
         },
